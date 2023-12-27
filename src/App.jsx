469c90f5a476828fb35f4b0c4dd16a5bffbaef04
1OmarTarek1 <cr7omartarek@gmail.com>
2023-12-28 00:49:04 +0200

    edit basname in app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ const App = () => {
     // top in every component change )
     return(
         <>
-            <Router> 
+            <Router basename='/FutureFc'> 
                 
                 <Headroom>
                     <Header /> 
