c1b1bef61ffcc77656912bc03a0157bb57c70b5a
1OmarTarek1 <cr7omartarek@gmail.com>
2024-01-19 22:33:14 +0200

    some edit

--- src/sections/Players/Players.js
+++ src/sections/Players/Players.js
@@ -1,12 +1,50 @@
 import { SectionHeader, SectionWrapper, BestPlayerCard } from '../../components'
 import  BestPlayersData from '../../Data/BestPlayersData' 
+import Slider from "react-slick";
+
 import './Players.css'
 
 
 import React from 'react'
 
 const Players = () => {
-
+    var settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        rtl: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                dots: false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+                }
+            }
+        ]
+    };
     const cards = BestPlayersData.map(card => {
         return <BestPlayerCard 
                 key   = { card.id    } 
@@ -24,7 +62,9 @@ return (
     <SectionWrapper>
         <SectionHeader>انتقالات اللاعبين</SectionHeader>
         <div className='bestPlayersCards'>
-            {cards}
+            <Slider {...settings}>
+                {cards}
+            </Slider>
         </div>
     </SectionWrapper>
     </div>
